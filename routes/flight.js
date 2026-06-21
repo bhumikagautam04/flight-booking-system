aa8d0dc74d03e8a6669d772f13f08257a55457b0
bhumikagautam04 <bhumikagautam751@gmail.com>
2026-06-21 18:17:41 +0530

    post-data

--- routes/flight.js
+++ routes/flight.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var pool = require('./pool');
 var router =express.Router();
+var upload=require('./multer');
 
 router.get('/flight_interface', function(req,res,next){
     res.render('flight_interface',{message:""});
@@ -18,15 +19,22 @@ router.get("/fetch_all_city", function(req,res){
     }
     })
 })
-router.post("/submit_flight_information",function(req,res){
-    pool.query("insert into flight(flight_name, flight_type, flight_seat,  source_city, departure_time, destination_city, arrival_time, company) values(?,?,?,?,?,?,?,?)", 
-    [req.body.flightname, req.body.flighttype, req.body.flightseats, req.body.sourcecity, req.body.departuretime, req.body.destinationcity, req.body.arrivaltime, req.body.company],
+router.post("/submit_flight_information", upload.single('picture'), function(req,res){
+        var days = req.body.days ? req.body.days.join(',') : '';
+        var logo = req.file ? req.file.filename : '';
+
+    pool.query("insert into flight(flight_name, flight_type, flight_seat,days, source_city, departure_time, destination_city, arrival_time, company,logo) values(?,?,?,?,?,?,?,?,?,?)", 
+    [req.body.flightname, req.body.flighttype, req.body.flightseats, days, req.body.sourcecity, req.body.departuretime, req.body.destinationcity, req.body.arrivaltime, req.body.company, logo],
     function(err,result){
         if(err){
+            console.log("Days:", req.body.days);
+            console.log("Joined Days:", days);
+            console.log("File:", req.file);
             res.json({status:false, message:"sql Error"});
         } else {
             res.json({status:true, message:"Flight added successfully"});
         }
     });
 })
+
 module.exports = router;
